a46de9b7805990f254dac42795e46deb5354d20b
alexsonalmeida <101877352+alexsonalmeida@users.noreply.github.com>
2022-05-17 19:11:55 -0300

    Update mongo.connection.js

--- crud-express/db/mongo.connection.js
+++ crud-express/db/mongo.connection.js
@@ -6,7 +6,7 @@ var mongoose = require('mongoose');
 var mongoDB_URI = 'mongodb://127.0.0.1:27017/university';
 
 mongoose.connect(mongoDB_URI,{useNewUrlParser:true});
-//mongoose.connect(mongoDB_URL, {userNewUrlParser:true});
+
 //armazena a conexão em uma variável
 var db = mongoose.connection;
 
@@ -22,17 +22,3 @@ db.on('disconnected',() => {
 db.on('error',(err) => {
  console.log('Mongoose Error: '+err);
 });
-/*
-//listeners professors
-db.on('connected',() => {
-    console.log('Mongoose Connected to '+mongoDB_URL);
-});
-   
-db.on('disconnected',() => {
-    console.log('Mongoose Disconnected to '+mongoDB_URL);
-});
-   
-db.on('error',(err) => {
-    console.log('Mongoose Error: '+err);
-});
-*/
